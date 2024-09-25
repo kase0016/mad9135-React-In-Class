e7b83d2ab264399800c80d0af032f649e65be645
Jean-Baptiste Kasenda <kase0016@algonquinlive.com>
2024-09-25 00:23:27 -0400

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,69 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import {
+  FlatList,
+  SafeAreaView,
+  ScrollView,
+  StyleSheet,
+  Text,
+  View,
+} from "react-native";
+import axios from "axios";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import { useState, useEffect } from "react";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+  const [todos, setTodos] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("https://jsonplaceholder.typicode.com/todos")
+      .then((response) => {
+        setTodos(response.data);
+      })
+      .catch((e) => {
+        console.error("Error fetching data: ", e);
+      });
+  });
+
+  const renderTodo = ({ item }) => (
+    <View style={styles.toDoList}>
+      <Text style={styles.title}>{item.title}</Text>
+      <Text style={styles.completed}>{item.completed}</Text>
     </View>
+  );
+
+  const keyExtractor = (item) => item.id.toString();
+
+  return (
+    <SafeAreaProvider style={styles.container}>
+      <ScrollView>
+        <SafeAreaView>
+          <FlatList
+            data={todos}
+            renderItem={renderTodo}
+            keyExtractor={keyExtractor}
+            style={styles.flatListStyle}
+          />
+        </SafeAreaView>
+      </ScrollView>
+    </SafeAreaProvider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
+  toDoList: {
+    display: "flex",
+    flexDirection: "column",
+    gap: "2",
+    marginBottom: 20,
+  },
+  title: { color: "black" },
+  flatListStyle: { paddingTop: 20, paddingBottom: 20, paddingLeft: 20 },
+  completed: {},
 });
